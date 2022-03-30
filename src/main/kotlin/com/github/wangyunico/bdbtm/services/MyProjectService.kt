ca0e77f32c6ffa69f770373497dff7f38013947c
GitHub Action <action@github.com>
2022-03-30 02:42:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/wangyunico/bdbtm/services/MyProjectService.kt
+++ src/main/kotlin/com/github/wangyunico/bdbtm/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.wangyunico.bdbtm.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.wangyunico.bdbtm.MyBundle
 
 class MyProjectService(project: Project) {
 
